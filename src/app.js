0ccca5f36e6a18d06fc19bb78d7771eb77043995
sushantbasak <sushantbasak73@gmail.com>
2021-05-20 15:37:45 +0530

    Hot fix port

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Server is running');
