c9c8ba64b34f38cebeb84be505ec55dd670ea723
sushantbasak <sushantbasak73@gmail.com>
2021-10-02 00:05:07 +0530

    feat: Add Twitter API Logic

--- src/app.js
+++ src/app.js
@@ -2,8 +2,48 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
+const axios = require('axios');
 
 const port = process.env.PORT || 3000;
+
+const token = process.env.AUTH_KEY;
+const apiUrl = process.env.API_URL;
+
+const config = {
+  headers: { Authorization: `Bearer ${token}` },
+};
+
+app.get('/twitter/rewards/:username', async (req, res) => {
+  const userName = req.params.username;
+
+  try {
+    const userData = await axios.get(apiUrl, config);
+
+    const finalData = userData.data.users;
+
+    let result = false;
+
+    finalData.forEach((data) => {
+      if (data.screen_name === userName) result = true;
+    });
+
+    if (result)
+      return res.send({
+        status: 'success',
+        message: 'Twitter Following Succcessful',
+      });
+    else
+      return res.send({
+        status: 'success',
+        message: 'User has not Followed yet',
+      });
+  } catch (e) {
+    console.log(e);
+    res
+      .status(400)
+      .send({ status: 'error', message: 'Something wrong Happened' });
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Server is running');
