82d468d694307b91d42143ecbd2990ff058fc2da
sushantbasak <sushantbasak73@gmail.com>
2021-10-02 00:18:03 +0530

    feat: Add User Follow in Twitter by their IDs

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 
 const token = process.env.AUTH_KEY;
 const apiUrl = process.env.API_URL;
+const apiUrl2 = process.env.API_URL2;
 
 const config = {
   headers: { Authorization: `Bearer ${token}` },
@@ -25,6 +26,38 @@ app.get('/twitter/rewards/:username', async (req, res) => {
 
     finalData.forEach((data) => {
       if (data.screen_name === userName) result = true;
+    });
+
+    if (result)
+      return res.send({
+        status: 'success',
+        message: 'Twitter Following Succcessful',
+      });
+    else
+      return res.send({
+        status: 'success',
+        message: 'User has not Followed yet',
+      });
+  } catch (e) {
+    console.log(e);
+    res
+      .status(400)
+      .send({ status: 'error', message: 'Something wrong Happened' });
+  }
+});
+
+app.get('/twitter/verifyuser/:id', async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const userData = await axios.get(apiUrl2, config);
+
+    const finalData = userData.data.ids;
+
+    let result = false;
+
+    finalData.forEach((data) => {
+      if (data == userId) result = true;
     });
 
     if (result)
